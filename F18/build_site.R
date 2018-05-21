055404b15f11a9a926b0ae7c9f0950311f032130
ovitek <o.vitek@neu.edu>
2018-05-21 10:03:42 -0400

    1
    1

--- F18/build_site.R
+++ F18/build_site.R
@@ -2,7 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("/Users/olgavitek/Dropbox/Olga/Teaching/DS5220/Fall17/DS5220/F17")
+setwd("/Users/olgavitek/Documents/GitHub/DS5220/F18")
 
 #render your sweet site. 
 rmarkdown::render_site()
